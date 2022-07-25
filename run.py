04759498762665c75e5cd088a0d0e3ed6961f8de
Jacob-Yearoo <jacob.yearoo@hotmail.com>
2022-07-25 19:57:30 +0000

    connect gitpod to worksheet and start first function to get the user input to start the program

--- run.py
+++ run.py
@@ -11,3 +11,23 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('BlockBuster')
+
+
+def get_user_input():
+    """
+    
+    """
+    # while True:
+    print("Please choose what you would like to do\n")
+    print("1:Check in-store stock\n")
+    print("2:Add or remove in-store stock\n")
+    print("3:Check In-store stock\n")
+
+    choice_input = input("Please enter the corresponding number.\n")
+
+    choice = int(choice_input)
+
+    print(f"You chose option {choice}")
+
+
+get_user_input()
